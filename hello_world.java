c7e91537d8ee2f7579a43c7abdbea114bf84e305
langfeng-tg <760295347@qq.com>
2019-11-29 11:23:11 +0800

    Add Hello World by Java

---
+++ hello_world.java
@@ -0,0 +1,19 @@
+package tengguo;
+class Animalssss{
+	int a=1;
+	public static void animalShout() {
+		System.out.println("这是第一个JAVA程序");
+	}
+}
+
+public class Example01 {
+	public static void main (String[] args) {
+		Animalssss p=new Animalssss
+				();
+		p.animalShout();
+		 System.out.println("这是第一个JAVA程序");
+		//功能执行语句后需要加;（英文）.
+		//连续字符串不能分成两行书写.但是可用+将两个字符串串联起来，如Example02
+	}
+	
+}
